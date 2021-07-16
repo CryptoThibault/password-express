ca3f3c7fe3a700bdb1ae89ec6d36df43282c9e1e
CryptoThibault <thibault.chalaoux@gmail.com>
2021-07-16 09:29:48 +0200

    post

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const fsPromises = require('fs/promises')
 const ethers = require('ethers')
 const app = express()
 
+const db_user = {
+  alice: '123',
+  bob: '456',
+  charlie: '789',
+}
+
 // Middleware for checking if user exists
 const userChecker = (req, res, next) => {
   const username = req.body.username
@@ -25,7 +31,7 @@ const passwordChecker = (req, res, next) => {
 }
 
 const PORT = 3333;
-const IP_LOOPBACK = 'localhost'
+const IP = '192.168.1.82'
 const LOG_FILE = 'log.json'
 const FORMAT_FILE = 'utf-8'
 
@@ -33,25 +39,19 @@ app.get('/', async (req, res) => {
   res.send(`Welcome to your local server at PORT ${PORT}`)
 });
 
-app.post('/register/:username/:password', async (req, res) => {
-  if (!userChecker(req.params.username)) {
-    const log = `"${req.params.username}": ${ethers.utils.keccak256(req.params.password)}`
-    await fsPromises.appendFile(LOG_FILE, JSON.stringify(log), FORMAT_FILE)
-    res.send(`Your register as ${req.params.username}`)
-  } else {
-    res.send(`The username ${req.params.username} already use`)
-  }
-});
+app.use('/register', !userChecker)
+app.use('/login', userChecker)
+app.use('/login', passwordChecker)
 
-app.post('/login/:username/:password', async (req, res) => {
-  const log = JSON.parse(await fsPromises.readFile(LOG_FILE, FORMAT_FILE))
-  if (userChecker(req) && passwordChecker(req)) {
-    res.send(`Welcome to your dahsboard ${req.params.username}`)
-  } else {
-    res.send('Username or Password incorect')
-  }
-});
+app.post('/register', async (req, res) => {
+  const log = `"${req.body.username}": ${ethers.utils.keccak256(req.body.password)}`
+  await fsPromises.appendFile(LOG_FILE, log, FORMAT_FILE)
+  res.send(`Your register as ${req.body.username}`)
+})
+app.post('/login', (req, res) => {
+  res.send(`Welcome to your dahsboard ${req.body.username}`)
+})
 
-app.listen(PORT, async () => {
-  console.log(`App listening at http://${IP_LOOPBACK}:${PORT}`)
+app.listen(PORT, IP, async () => {
+  console.log(`App listening on ${IP}:${PORT}`)
 });
